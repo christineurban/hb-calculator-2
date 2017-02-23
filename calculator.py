e1d805b1be00fe6051d50a76133bf45dbba2fe22
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-23 13:27:47 -0800

    Added calculator.py file, WIP: preventing user from breaking calculator

--- calculator.py
+++ calculator.py
@@ -7,6 +7,12 @@ calculator program yourself in this file.
 
 from arithmetic import *  #importing everything from arithmetic.py file
 
+def is_valid_operator:
+    if tokens[0] !== "+" or !== "-" or !== "*" or !== "/" or !== "square" or !== "cube" or !== "pow" or !== "mod":
+        print "You have entered an invalid operation. Please try again."
+        continue
+
+
 
 using_calculator = True  #while user is inputting operators and numbers
 
@@ -16,35 +22,41 @@ while using_calculator:
     user_math = raw_input()  #stores input from user as a string
 
     tokens = user_math.split(" ")  #splits string into list of strings
-
-    num1 = int(tokens[1])  #converts first index from a string to an integer
     
-    if tokens.len == 3:  #checks if third token exists in list
-        num2 = int(tokens[2])  #converts second index from a string to an integer
+    is_valid_operator(tokens)
+
+    user_numbers = []
+    
+    for expected_number in tokens[1:]:
+        if expected_number.isdigit():
+            user_numbers.append(int(tokens[i]))  #converts each index from [1] from a string to an integer
+        else:
+            print "You have entered an a non-numeric value. Please try again."
+            continue 
 
     if tokens[0] == "+":
-        print add(num1, num2)
+        print add(user_numbers)
 
     elif tokens[0] == "-":
-        print subtract(num1, num2)
+        print subtract(user_numbers)
 
     elif tokens[0] == "*":
-        print multiply(num1, num2)
+        print multiply(user_numbers)
 
     elif tokens[0] == "/":
-        print divide(float(num1), float(num2))
+        print divide(user_numbers)
 
     elif tokens[0] == "square":
-        print square(num1)
+        print square(user_numbers)
 
     elif tokens[0] == "cube":
-        print cube(num1)
+        print cube(user_numbers)
 
     elif tokens[0] == "pow":
-        print power(num1, num2)
+        print power(user_numbers)
 
     elif tokens[0] == "mod":
-        print mod(num1, num2)
+        print mod(user_numbers)
 
     elif tokens[0] == "q" or tokens[0] == "quit":
         using_calculator = False
