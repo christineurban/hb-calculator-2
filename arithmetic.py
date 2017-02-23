14c83591564e7bc1452084cd1a05e75e602b18a1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-22 18:06:49 -0800

    WIP: Make a new arithmetic.py with functions that take any number of number inputs

--- arithmetic.py
+++ arithmetic.py
@@ -1,16 +1,27 @@
-def add(num1, num2):
-    return num1 + num2
+def add(user_numbers):
+    running_total = 0
+
+    for number in user_numbers:
+        running_total += number
+
+    return running_total 
+            
 
 
-def subtract(num1, num2):
-    return num1 - num2
+def subtract(user_numbers):
+    running_total = user_numbers[0]
+
+    for number in range(1, (len(user_numbers) - 1)):
+        running_total -= number
+
+    return running_total 
 
 
-def multiply(num1, num2):
+def multiply(user_numbers):
     return num1 * num2
 
 
-def divide(num1, num2):
+def divide(user_numbers):
     # Need to turn at least argument to float for division to
     # not be integer division
     return float(num1) / float(num2) 
@@ -26,9 +37,9 @@ def cube(num1):
     return num1 * num1 * num1
 
 
-def power(num1, num2):
+def power(user_numbers):
     return num1 ** num2  # ** = exponent operator
 
 
-def mod(num1, num2):
+def mod(user_numbers):
     return num1 % num2
